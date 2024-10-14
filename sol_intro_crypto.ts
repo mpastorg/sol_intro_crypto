2ef8ad912330385a50176fdff31c83e7b87e4ab9
Marcos Pastor <marcos@pastorg.es>
2024-10-14 06:28:48 +0100

    added connections local or devnet

--- sol_intro_crypto.ts
+++ sol_intro_crypto.ts
@@ -1,6 +1,25 @@
-import { Keypair } from "@solana/web3.js";
-import "dotenv/config";
+import { Keypair, Connection, clusterApiUrl, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import * as dotenv from 'dotenv';
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+dotenv.config();
+
+const rpcUrl = process.env.SOLANA_RPC_URL || "https://api.devnet.solana.com";
+const connection = new Connection(rpcUrl, "confirmed");
+const address = new PublicKey(process.env.LOCAL_PUB_KEY || "BbMwxEkVi84w476HmzJHSbs5FSU2haJwPQL6Ezi43bnu");
+
+var suppliedPublicKey = process.argv[2];
+var publicKey;
+
+if (!suppliedPublicKey) {
+    publicKey = address;
+} else {
+    publicKey = new PublicKey(suppliedPublicKey);
+}
+const balance = await connection.getBalance(publicKey);
+console.log(`balance of account ${publicKey} is ${balance} lamports and ${balance / LAMPORTS_PER_SOL} SOL`);
+
+
 
 var keypair = Keypair.generate();
 console.log('Generated keypair!');
